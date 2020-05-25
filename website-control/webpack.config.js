8973d86e42e6adba793d3e4986a1feadfec3e27c
Tobias Möhring <dasbente@gmail.com>
2020-05-25 13:25:26 +0200

    Overhaul frontend development backbone

---
+++ website-control/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: path.join(__dirname, 'js', 'bundle.js'),
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+
+  resolve: {
+    alias: {
+      actions: path.resolve(__dirname, 'src', 'actions'),
+      components: path.resolve(__dirname, 'src', 'components'),
+      reducers: path.resolve(__dirname, 'src', 'reducers'),
+      utilities: path.resolve(__dirname, 'src', 'utilities')
+    },
+
+    extensions: ['*', '.js', '.jsx']
+  },
+
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '.')
+  }
+}
