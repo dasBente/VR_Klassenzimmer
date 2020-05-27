f46a516bfe1eba00eccc0ae0ccaee9b10c155344
Tobias Möhring <dasbente@gmail.com>
2020-05-27 08:15:19 +0200

    Use socket.io API for IPC

---
+++ Assets/ownScripts/SocketServer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+public class SocketServer : WebSocketBehavior
+{
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Debug.Log("Server read " + e.Data);
+        var msg = e.Data == "BALUS"
+            ? "I've been balused already..."
+            : "I'm not available now.";
+        Send(msg);
+    }
+
+    public static WebSocketServer HostServer()
+    {
+        var wssv = new WebSocketServer("ws://localhost:8080");
+        wssv.AddWebSocketService<SocketServer>("/SockServer");
+        wssv.Start();
+        Debug.Log("Start Socket Server");
+        return wssv;
+    }
+}
